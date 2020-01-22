edbd66b59fcfe8a7fbd06d3b847b7d3e011ba4ba
ray0728 <37318939+ray0728@users.noreply.github.com>
2020-01-22 18:04:21 +0800

    Create build_ffmpeg_for_android.sh

---
+++ bash/build_ffmpeg_for_android.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+export TMPDIR=/d/Users/twx808015/Documents/tmp/ffmpeg/temp
+#你自己的NDK路径.
+export NDK=/d/android-ndk-r20b-windows-x86_64/android-ndk-r20b
+TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/windows-x86_64
+API=29
+
+function build_android
+{
+echo "Create FFmpeg configuration files for $CPU"
+./configure \
+    --prefix=$PREFIX \
+    --enable-neon \
+    --enable-hwaccels \
+    --enable-gpl \
+    --enable-postproc \
+    --enable-shared \
+    --enable-jni \
+    --enable-mediacodec \
+    --enable-decoder=h264_mediacodec \
+    --disable-static \
+    --disable-doc \
+    --disable-ffmpeg \
+    --disable-ffplay \
+    --disable-ffprobe \
+    --enable-avdevice \
+    --disable-doc \
+    --disable-symver \
+    --cross-prefix=$CROSS_PREFIX \
+    --target-os=android \
+    --arch=$ARCH \
+    --cpu=$CPU \
+    --cc=$CC
+    --cxx=$CXX
+    --enable-cross-compile \
+    --sysroot=$SYSROOT \
+    --extra-cflags="-Os -fpic $OPTIMIZE_CFLAGS" \
+    --extra-ldflags="$ADDI_LDFLAGS" \
+    $ADDITIONAL_CONFIGURE_FLAG
+echo "Clean FFmpeg configuation files for $CPU"
+make clean
+echo "Compiling FFmpeg for $CPU"
+make -j4
+make install
+echo "The Compilation of FFmpeg for $CPU is completed"
+}
+
+# armv8-a
+ARCH=arm64
+CPU=armv8-a
+CC=$TOOLCHAIN/bin/aarch64-linux-android$API-clang
+CXX=$TOOLCHAIN/bin/aarch64-linux-android$API-clang++
+SYSROOT=$TOOLCHAIN/sysroot
+CROSS_PREFIX=$TOOLCHAIN/bin/aarch64-linux-android-
+PREFIX=$(pwd)/android/arm64-v8a
+OPTIMIZE_CFLAGS="-march=$CPU"
+build_android
+
+#armv7-a
+ARCH=arm
+CPU=armv7-a
+CC=$TOOLCHAIN/bin/armv7a-linux-androideabi$API-clang
+CXX=$TOOLCHAIN/bin/armv7a-linux-androideabi$API-clang++
+SYSROOT=$TOOLCHAIN/sysroot
+CROSS_PREFIX=$TOOLCHAIN/bin/arm-linux-androideabi-
+PREFIX=$(pwd)/android/armeabi-v7a
+OPTIMIZE_CFLAGS="-mfloat-abi=softfp -mfpu=vfp -marm -march=$CPU "
+build_android
+
+# x86
+# ARCH=x86
+# CPU=x86
+# CC=$TOOLCHAIN/bin/i686-linux-android$API-clang
+# CXX=$TOOLCHAIN/bin/i686-linux-android$API-clang++
+# SYSROOT=$NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot
+# CROSS_PREFIX=$TOOLCHAIN/bin/i686-linux-android-
+# PREFIX=$(pwd)/android/$CPU
+# OPTIMIZE_CFLAGS="-march=i686 -mtune=intel -mssse3 -mfpmath=sse -m32"
+# build_android
+
+# x86_64
+# ARCH=x86_64
+# CPU=x86-64
+# CC=$TOOLCHAIN/bin/x86_64-linux-android$API-clang
+# CXX=$TOOLCHAIN/bin/x86_64-linux-android$API-clang++
+# SYSROOT=$NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot
+# CROSS_PREFIX=$TOOLCHAIN/bin/x86_64-linux-android-
+# PREFIX=$(pwd)/android/$CPU
+# OPTIMIZE_CFLAGS="-march=$CPU -msse4.2 -mpopcnt -m64 -mtune=intel"
+# build_android

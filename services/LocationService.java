77128ba709772cac20eb3fbbaf1ae9c726a5d73a
ray0728 <37318939+ray0728@users.noreply.github.com>
2020-02-29 18:57:54 +0800

    Update LocationService.java

--- services/LocationService.java
+++ services/LocationService.java
@@ -1,49 +1,54 @@
 package com.ray.lab.voice.services;
 
-import android.annotation.TargetApi;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Binder;
-import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
-import android.os.PowerManager;
-import android.util.Log;
+import android.os.Message;
+import android.util.SparseArray;
+
 
 import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
-import com.amap.api.location.AMapLocationQualityReport;
+import com.ray.lab.voice.MainActivity;
 import com.ray.lab.voice.R;
+import com.ray.lab.voice.StatusActivity;
+import com.ray.lab.voice.services.options.LocationOptions;
+import com.ray.lab.voice.storage.LocationStorage;
 import com.ray.lab.voice.util.DeviceInfo;
 import com.ray.lab.voice.util.HttpHelper;
+import com.ray.lab.voice.util.LocationData;
+import com.ray.lab.voice.util.Toolkit;
 
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class LocationService extends Service implements AMapLocationListener, HttpHelper.HttpListener {
     private static final String TAG = "LocationService";
-    private static final String NOTIFICATION_CHANNEL_NAME = "VOICE_LOCTION";
-    private static SimpleDateFormat mSimpleDateFormat = null;
-    private boolean isCreateChannel = false;
+    public static final String KEY_LOCATION_OPTIONS = "LocationOptions";
+    private static final int DEFAULT_NOTIFY_ID = 2;
+    private static final String CHANNEL_ID = "com.ray.lab.voice.location";
+    private static final int MESSAGE_CALL_PHONE = 1;
+    private static final int MESSAGE_CALL_APP = 2;
     private AMapLocationClient mLocationClient = null;
-    private PowerManager.WakeLock mWakeLock = null;
-    private NotificationManager mNotificationManager = null;
     private final IBinder mBinder = new LocalBinder();
     private HttpHelper mHttpHelper = null;
-
+    private LocationStorage mStorage = null;
+    private double[] mLastLocation = new double[2];
+    private Intent mMainUIIntent = new Intent();
+    private List<ScheduledExecutorService> mExecutorList = new ArrayList<>();
 
     @Override
     public void onCreate() {
         super.onCreate();
-        if (Build.VERSION.SDK_INT >= 26) {
-            showNotify();
-        }
-        initLocation();
+        mLastLocation[0] = 0;
+        mLastLocation[1] = 0;
     }
 
     @Override
@@ -51,81 +56,79 @@ public class LocationService extends Service implements AMapLocationListener, Ht
         return mBinder;
     }
 
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        acquireWakeLock();
-        if (mLocationClient != null && mLocationClient.isStarted()) {
-            mLocationClient.startLocation();
+        mMainUIIntent.setAction(StatusActivity.ACTION);
+        mMainUIIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        final LocationOptions options = (LocationOptions) intent.getSerializableExtra(KEY_LOCATION_OPTIONS);
+        initLocation(options.build());
+        ScheduledExecutorService executor;
+        if (options.hasPhoneOptions()) {
+            executor = Executors.newSingleThreadScheduledExecutor();
+            executor.scheduleAtFixedRate(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(mMainUIIntent);
+                    Toolkit.self().autoCallAndEnd(getApplicationContext(), options.getPhoneNumber(), options.getCallDelay());
+                }
+            }, 1, options.getCallIntervals(), TimeUnit.SECONDS);
+            mExecutorList.add(executor);
         }
+        if (options.hasThirdAppOptions()) {
+            executor = Executors.newSingleThreadScheduledExecutor();
+            executor.scheduleAtFixedRate(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(mMainUIIntent);
+                    Toolkit.self().startThirdApp(getApplicationContext(), options.getThirdAppPkgName());
+                }
+            }, 1, options.getCallThirdIntervals(), TimeUnit.SECONDS);
+        }
+//        Toolkit.showForegroundNotifiy(this, DEFAULT_NOTIFY_ID, CHANNEL_ID, TAG, null, R.string.gnss_trace, R.string.gnss_trace);
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
-        super.onDestroy();
-        releaseWakeLock();
+        if (mStorage != null) {
+            mStorage.stop();
+        }
+        for(ScheduledExecutorService executor: mExecutorList){
+            executor.shutdown();
+        }
         destroyLocation();
+        super.onDestroy();
     }
 
-    private void acquireWakeLock() {
-        if (null == mWakeLock) {
-            PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-            mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK
-                    | PowerManager.ON_AFTER_RELEASE, getClass()
-                    .getCanonicalName());
-            if (null != mWakeLock) {
-                Log.d(TAG, "call acquireWakeLock");
-                mWakeLock.acquire();
-            }
+    private void initStorage() {
+        if (mStorage == null) {
+            mStorage = new LocationStorage(getApplicationContext(), "Location");
         }
     }
 
-    private void releaseWakeLock() {
-        if (null != mWakeLock && mWakeLock.isHeld()) {
-            Log.d(TAG, "call releaseWakeLock");
-            mWakeLock.release();
-            mWakeLock = null;
-        }
+    public SparseArray<LocationData> getAllRecords(String date) {
+        initStorage();
+        return mStorage.getRecord(date);
     }
 
-    public void showNotify() {
-        Notification.Builder builder = null;
-        Notification notification = null;
-        if (android.os.Build.VERSION.SDK_INT >= 26) {
-            if (null == mNotificationManager) {
-                mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            }
-            String channelId = getPackageName();
-            if (!isCreateChannel) {
-                NotificationChannel notificationChannel = new NotificationChannel(channelId,
-                        NOTIFICATION_CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
-                notificationChannel.enableLights(false);
-                notificationChannel.setLightColor(Color.BLUE);
-                notificationChannel.setShowBadge(true);
-                mNotificationManager.createNotificationChannel(notificationChannel);
-                isCreateChannel = true;
-            }
-            builder = new Notification.Builder(getApplicationContext(), channelId);
-        } else {
-            builder = new Notification.Builder(getApplicationContext());
-        }
-        builder.setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("后台定位服务")
-                .setContentText("")
-                .setWhen(System.currentTimeMillis());
-        if (android.os.Build.VERSION.SDK_INT >= 16) {
-            notification = builder.build();
-        } else {
-            notification = builder.getNotification();
-        }
-        startForeground(110, notification);
+    public SparseArray<LocationData> getAllLocationRecords(long start, long end) {
+        initStorage();
+        return mStorage.getLocationRecord(start, end);
     }
 
     @Override
     public void onLocationChanged(AMapLocation location) {
-        if (null != location && location.getErrorCode() == 0) {
-            mHttpHelper.get(DeviceInfo.self().toLocalString("https://www.iamray.cn/rst/lab/voice/device", location.getLatitude(), location.getLongitude()));
+        String locationType = "undefine";
+        if (null != location && location.getErrorCode() == AMapLocation.LOCATION_SUCCESS) {
+            if (location.getLatitude() == mLastLocation[0] && location.getLongitude() == mLastLocation[1]) {
+                return;
+            }
+            mLastLocation[0] = location.getLatitude();
+            mLastLocation[1] = location.getLongitude();
+            mHttpHelper.put("https://www.iamray.cn/rst/lab/voice/device", DeviceInfo.self().toLocalString(location.getLatitude(), location.getLongitude()));
+            initStorage();
+            mStorage.record(Toolkit.formateDate(Toolkit.FORMAT_DATE_YMD_HMS, null), location.getLocationType(), location.getLatitude(), location.getLongitude());
         }
     }
 
@@ -145,51 +148,13 @@ public class LocationService extends Service implements AMapLocationListener, Ht
         }
     }
 
-    private void initLocation() {
+    private void initLocation(AMapLocationClientOption option) {
         mHttpHelper = new HttpHelper(this);
         mLocationClient = new AMapLocationClient(this.getApplicationContext());
-        mLocationClient.setLocationOption(getDefaultOption());
+        mLocationClient.setLocationOption(option);
         mLocationClient.setLocationListener(this);
         mLocationClient.stopLocation();
         mLocationClient.startLocation();
-    }
-
-    private AMapLocationClientOption getDefaultOption() {
-        AMapLocationClientOption option = new AMapLocationClientOption();
-        option.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);//可选，设置定位模式，可选的模式有高精度、仅设备、仅网络。默认为高精度模式
-        option.setGpsFirst(false);//可选，设置是否gps优先，只在高精度模式下有效。默认关闭
-        option.setHttpTimeOut(30000);//可选，设置网络请求超时时间。默认为30秒。在仅设备模式下无效
-        option.setInterval(2000);//可选，设置定位间隔。默认为2秒
-        option.setNeedAddress(true);//可选，设置是否返回逆地理地址信息。默认是true
-        option.setOnceLocation(false);//可选，设置是否单次定位。默认是false
-        option.setOnceLocationLatest(false);//可选，设置是否等待wifi刷新，默认为false.如果设置为true,会自动变为单次定位，持续定位时不要使用
-        AMapLocationClientOption.setLocationProtocol(AMapLocationClientOption.AMapLocationProtocol.HTTP);//可选， 设置网络请求的协议。可选HTTP或者HTTPS。默认为HTTP
-        option.setSensorEnable(false);//可选，设置是否使用传感器。默认是false
-        option.setWifiScan(true); //可选，设置是否开启wifi扫描。默认为true，如果设置为false会同时停止主动刷新，停止以后完全依赖于系统刷新，定位位置可能存在误差
-        option.setLocationCacheEnable(true); //可选，设置是否使用缓存定位，默认为true
-        return option;
-    }
-
-    private String getGPSStatusString(int statusCode) {
-        String str = "";
-        switch (statusCode) {
-            case AMapLocationQualityReport.GPS_STATUS_OK:
-                str = "GPS状态正常";
-                break;
-            case AMapLocationQualityReport.GPS_STATUS_NOGPSPROVIDER:
-                str = "手机中没有GPS Provider，无法进行GPS定位";
-                break;
-            case AMapLocationQualityReport.GPS_STATUS_OFF:
-                str = "GPS关闭，建议开启GPS，提高定位质量";
-                break;
-            case AMapLocationQualityReport.GPS_STATUS_MODE_SAVING:
-                str = "选择的定位模式中不包含GPS定位，建议选择包含GPS定位的模式，提高定位质量";
-                break;
-            case AMapLocationQualityReport.GPS_STATUS_NOGPSPERMISSION:
-                str = "没有GPS定位权限，建议开启gps定位权限";
-                break;
-        }
-        return str;
     }
 
     private void destroyLocation() {
@@ -200,5 +165,4 @@ public class LocationService extends Service implements AMapLocationListener, Ht
             mLocationClient = null;
         }
     }
-
 }
